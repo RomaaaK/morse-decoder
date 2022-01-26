80f4a868c1573def4142b6910b5e1f27ce9779df
RomaaaK <zimpsonzya@gmail.com>
2022-01-26 23:44:28 +0300

    feat: implement function morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const phrase = []
+    let encodedLetter
+    let letter
+    let partLetter
+    
+    for (let i = 0; i < expr.length / 10; i ++) {
+        encodedLetter = expr.slice(i * 10, (i * 10) + 10)
+        letter = []
+ 
+        if (encodedLetter[0] === '*') {
+            phrase.push(' ')
+            continue
+        }
+ 
+        for (let y = 0; y < 5; y++) {
+            partLetter = encodedLetter.slice(y * 2, (y * 2) + 2)
+ 
+            if (partLetter == '10') {
+                letter.push('.')
+                continue
+            }
+ 
+            if (partLetter == '11') {
+                letter.push('-')
+            } 
+        }
+ 
+        phrase.push(MORSE_TABLE[letter.join('')])
+    }
+ 
+    return phrase.join('')
 }
 
 module.exports = {
